f3b8159fd3a207f3b38916c26ee4238516b39838
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-19 23:54:31 -0300

    parteCTerminado

--- LAB/obligatorio.c
+++ LAB/obligatorio.c
@@ -28,14 +28,14 @@
  *   Detiene la ejecución del programa.
  *
  */
-
+//parametros globales
 short AREA_DE_MEMORIA[2048];
 const short VACIO = 0x8000;
+short nodoDinamico = 0; //para parte dinámica
+
 //puertos
 const short PUERTO_ENTRADA = 20;
-
 const short PUERTO_SALIDA = 21;
-
 const short PUERTO_LOG = 22;
 
 //codigos de salida
@@ -48,7 +48,7 @@ const short CODIGO_NODO_EXISTENTE = 8;
 
 unsigned char eleccion ;
 char continuarPrograma = 1;
-short modo = 0; //indica modo estático o dinámico, comienza en estático
+short modo; //indica modo estático o dinámico, comienza en estático
 
 void inicializar_memoria(){
     for (short i = 0; i < 2048; i++){
@@ -76,7 +76,7 @@ void agregarNodo(short num, short nodo){ //insertarEnABB (2)
 }
 
 short calcularAltura(short nodo){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (3)
-    if (nodo < 2048 && AREA_DE_MEMORIA[nodo] != VACIO){
+    if (nodo <= 2048 && AREA_DE_MEMORIA[nodo] != VACIO){
         short altIzquierda = calcularAltura(2*(nodo+1)-1) + 1;
         short altDerecha = calcularAltura(2*(nodo+1)) + 1;
         if (altDerecha > altIzquierda)
@@ -119,32 +119,39 @@ void imprimirMemoria(short N){ //imprime el ABb, 1 mayor a menor, 0 menor a mayo
 //END Modo estático
 
 //Modo dinámico
-void agregarNodoDinamico(short num, short nodo){ //insertarEnABB (2)
-    if (nodo>=2048/3){
+void agregarNodoDinamico(short num, short pos){
+    if (pos > 2048/3){
         outputPuertoLog(CODIGO_ESCRITURA_INVALIDA);
         return;
     }
-    if (AREA_DE_MEMORIA[nodo] == VACIO && nodo==0){
-        AREA_DE_MEMORIA[nodo] = num;
+    if (AREA_DE_MEMORIA[pos] == VACIO) {
+        if (pos == 0) {
+            AREA_DE_MEMORIA[pos] = num;//nodo 0
+        } else {
+            nodoDinamico++;//incrementar nodo
+            AREA_DE_MEMORIA[pos] = nodoDinamico;
+            AREA_DE_MEMORIA[3*nodoDinamico] = num;
+        }
         outputPuertoLog(CODIGO_EXITO);
-    }else if (AREA_DE_MEMORIA[nodo] == VACIO){
-        AREA_DE_MEMORIA[nodo] = nodo;
-        AREA_DE_MEMORIA[3*nodo] = num;
-        outputPuertoLog(CODIGO_EXITO);
-    }else{
-        if(AREA_DE_MEMORIA[3*nodo]<num)
-            agregarNodo(num, 2*(nodo+1));
-        else if(AREA_DE_MEMORIA[3*nodo]>num)
-            agregarNodo(num, 2*(nodo+1)-1);
+    } else if (num < AREA_DE_MEMORIA[pos]) {
+        if (AREA_DE_MEMORIA[pos+1] != VACIO)
+            agregarNodoDinamico(num, 3*AREA_DE_MEMORIA[pos+1]);
         else
-            outputPuertoLog(CODIGO_NODO_EXISTENTE);
-    }
+            agregarNodoDinamico(num, pos+1);
+    } else if (num > AREA_DE_MEMORIA[pos]) {
+        if (AREA_DE_MEMORIA[pos+2] != VACIO)
+            agregarNodoDinamico(num, 3*AREA_DE_MEMORIA[pos+2]);
+        else    
+            agregarNodoDinamico(num, pos+2);
+    } else     
+        outputPuertoLog(CODIGO_NODO_EXISTENTE);
 }
 
-short calcularAlturaDinamico(short nodo){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (3)
-    if (nodo <= 2048/3 && AREA_DE_MEMORIA[nodo] != VACIO){
-        short altIzquierda = calcularAltura(3*(2*(nodo+1) - 1) + 1);
-        short altDerecha = calcularAltura(3*(2*(nodo+1)) + 1);
+
+short calcularAlturaDinamico(short pos){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (3)
+    if (pos != VACIO && AREA_DE_MEMORIA[pos] != VACIO && pos <= 2048/3 ){
+        short altIzquierda = calcularAlturaDinamico(3*AREA_DE_MEMORIA[pos+1]) + 1;
+        short altDerecha = calcularAlturaDinamico(3*AREA_DE_MEMORIA[pos+2]) + 1;
         if (altDerecha > altIzquierda)
             return altDerecha;
         else
@@ -153,32 +160,36 @@ short calcularAlturaDinamico(short nodo){ //imprime el ABb, 1 mayor a menor, 0 m
         return 0;
 }
 
-short calcularSumaDinamico(short nodo){ //Calcular suma (4)
+
+short calcularSumaDinamico(short pos){ //Calcular suma (4)
     short suma = 0;
-    if (nodo <= 2048/3 && AREA_DE_MEMORIA[nodo] != VACIO){
-        suma = AREA_DE_MEMORIA[3*nodo] + calcularSuma(2*(nodo+1)) + calcularSuma(2*(nodo+1)-1);
+    if (pos != VACIO && pos <= 2048 && AREA_DE_MEMORIA[pos] != VACIO){
+        suma = AREA_DE_MEMORIA[pos] + calcularSumaDinamico(3*AREA_DE_MEMORIA[pos+1]) + calcularSumaDinamico(3*AREA_DE_MEMORIA[pos+2]);
     }
     return suma;
 }
 
-void imprimirArbolDinamico(short nodo, short orden){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (5)
-    if (nodo < 2048 && AREA_DE_MEMORIA[nodo] != VACIO){
+void imprimirArbolDinamico(short pos, short orden){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (5)
+    if (pos != VACIO && pos <= 2048 && AREA_DE_MEMORIA[pos] != VACIO){
         if (orden == 1 ){
-            imprimirArbol(3*(2*(nodo+1)),orden);
-            outputPuertoSalida(AREA_DE_MEMORIA[nodo]);      
-            imprimirArbol(3*(2*(nodo+1)-1),orden);
+            imprimirArbolDinamico(3*AREA_DE_MEMORIA[pos+2],orden);
+            outputPuertoSalida(AREA_DE_MEMORIA[pos]);      
+            imprimirArbolDinamico(3*AREA_DE_MEMORIA[pos+1],orden);
         }
         else if(orden == 0){
-            imprimirArbol(3*(2*(nodo+1)-1),orden);
-            outputPuertoSalida(AREA_DE_MEMORIA[nodo]);      
-            imprimirArbol(3*(2*(nodo+1)),orden);
+            imprimirArbolDinamico(3*AREA_DE_MEMORIA[pos+1],orden);
+            outputPuertoSalida(AREA_DE_MEMORIA[pos]);      
+            imprimirArbolDinamico(3*AREA_DE_MEMORIA[pos+2],orden);
         }
     }
 }
 
 void imprimirMemoriaDinamico(short N){ //imprime el ABb, 1 mayor a menor, 0 menor a mayor (5)
-    for (short i = 0; i < N*3; i=(i+1)*3){//modo estático, donde cada posición supone simular
-        outputPuertoSalida(AREA_DE_MEMORIA[i]);   
+    for (short i = 0; i < 3*N; i+=3){//modo estático, donde cada posición supone simular
+        outputPuertoSalida(AREA_DE_MEMORIA[i]);
+        outputPuertoSalida(AREA_DE_MEMORIA[i+1]);
+        outputPuertoSalida(AREA_DE_MEMORIA[i+2]);
+        printf("\n");   
     }
 }
 //END Modo dinámico
@@ -203,6 +214,7 @@ int main() {
         switch (eleccion) {
             case 1: {
                 outputPuertoLog(1);
+                nodoDinamico = 0;
                 inicializar_memoria();
                 short num;
                 scanf("%hd", &num);//debe tomar la entrada de PS actual, lo simula con scanf
